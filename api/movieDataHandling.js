720b400ef646f937a6dcd45876186dad2c060bd3
Nguyễn Hoài Duy <hoaidii.it@gmail.com>
2024-04-10 23:28:55 +0700

    feat: add movie from API to Firebase

--- api/movieDataHandling.js
+++ api/movieDataHandling.js
@@ -1,5 +1,5 @@
 import { addDoc, doc, collection, setDoc  } from 'firebase/firestore';
-import { fetchTrendingMovies, fetchPopularMovies, fetchUpcomingMovies, fetchTopRatedMovies } from '../api/moviedb';
+import { fetchTrendingMovies, fetchPopularMovies, fetchUpcomingMovies, fetchTopRatedMovies, fetchMovieDetails, fetchMovieCredits, fetchMovieSimilar } from '../api/moviedb';
 import { firestore } from '../config/firebase';
 
 // Hàm để lưu thông tin bộ phim vào Firestore
@@ -10,6 +10,39 @@ const saveMovieToFirestore = async (movieData, source) => {
         console.log('Movie added to Firestore successfully');
     } catch (error) {
         console.error('Error adding movie to Firestore: ', error);
+    }
+}
+
+// Hàm để lưu thông tin movie details của bộ phim vào Firestore
+const saveMovieDetailsToFirestore = async (movieDetails) => {
+    try {
+        const movieDetailsRef = doc(firestore, "movieDetails", String(movieDetails.id));
+        await setDoc(movieDetailsRef, movieDetails);
+        console.log('Movie details added to Firestore successfully');
+    } catch (error) {
+        console.error('Error adding movie details to Firestore: ', error);
+    }
+}
+
+// Hàm để lưu thông tin credits của bộ phim vào Firestore
+const saveMovieCreditsToFirestore = async (movieId, creditsData) => {
+    try {
+        const movieCreditsRef = doc(firestore, "movieCredits", String(movieId));
+        await setDoc(movieCreditsRef, creditsData);
+        console.log('Movie credits added to Firestore successfully');
+    } catch (error) {
+        console.error('Error adding movie credits to Firestore: ', error);
+    }
+}
+
+// Hàm để lưu thông tin các bộ phim tương tự vào Firestore
+const saveSimilarMoviesToFirestore = async (movieId, similarMoviesData) => {
+    try {
+        const similarMoviesRef = doc(firestore, "similarMovies", String(movieId));
+        await setDoc(similarMoviesRef, similarMoviesData);
+        console.log('Similar movies added to Firestore successfully');
+    } catch (error) {
+        console.error('Error adding similar movies to Firestore: ', error);
     }
 }
 
@@ -34,6 +67,18 @@ export const handleMovieData = async () => {
                     vote_average: movie.vote_average,
                 };
                 await saveMovieToFirestore(movieData, source);
+
+                // Lấy thông tin chi tiết của bộ phim và lưu vào Firestore
+                const detailedMovieData = await fetchMovieDetails(movie.id);
+                await saveMovieDetailsToFirestore(detailedMovieData);
+
+                // Lấy thông tin credits của bộ phim và lưu vào Firestore
+                const creditsData = await fetchMovieCredits(movie.id);
+                await saveMovieCreditsToFirestore(movie.id, creditsData);
+
+                // Lấy thông tin các bộ phim tương tự và lưu vào Firestore
+                const similarMoviesData = await fetchMovieSimilar(movie.id);
+                await saveSimilarMoviesToFirestore(movie.id, similarMoviesData);
             }
         };
 
