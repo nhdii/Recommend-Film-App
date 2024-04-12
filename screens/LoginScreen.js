ef1250a444f48dcc9f143ab90b2fe8e2f45aadb4
Nguyễn Hoài Duy <hoaidii.it@gmail.com>
2024-04-12 22:03:40 +0700

    update: update KeyboardAvoidingView in LoginScreen and SignUpScreen

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { View, Text, TextInput, TouchableOpacity } from 'react-native';
+import { View, Text, TextInput, TouchableOpacity, Keyboard, KeyboardAvoidingView, Platform } from 'react-native';
 import { KeyIcon, EnvelopeIcon, ArrowLeftIcon, EyeIcon, EyeSlashIcon } from 'react-native-heroicons/outline';
 import { useNavigation } from '@react-navigation/native';
 import { styles } from '../theme';
@@ -60,90 +60,84 @@ export default function LoginScreen() {
         promptAsync();
     };
 
-    const handleLoginWithFacebook = () => {
-        // Xử lý đăng nhập bằng Facebook
-    };
-
     return (
-        <View className="flex-1 bg-neutral-800">
-            <SafeAreaView className="flex">
-                <View className="flex-row justify-start mt-2">
-                    <TouchableOpacity 
-                        onPress={()=> navigation.navigate("Home")}
-                        className="bg-yellow-400 p-2 rounded-tr-2xl rounded-bl-2xl ml-4">
-                        <ArrowLeftIcon size="20" color="black"/>
+        <KeyboardAvoidingView behavior={Platform.OS === 'ios' ? 'padding': 'height'} className="flex-1">
+            <View className="flex-1 bg-neutral-800">
+                <SafeAreaView className="flex-1">
+                    <View className="flex-row justify-start mt-2">
+                        <TouchableOpacity 
+                            onPress={()=> navigation.navigate("Home")}
+                            className="bg-yellow-400 p-2 rounded-tr-2xl rounded-bl-2xl ml-4">
+                            <ArrowLeftIcon size="20" color="black"/>
+                        </TouchableOpacity>
+                    </View>
+                </SafeAreaView>
+                <View className="mb-2 mt-3">
+                    <View className="flex-row justify-center items-center mx-4">
+                        <Text className="text-white text-3xl font-bold justify-center items-center">
+                            <Text style={styles.text}>W</Text>elcome!
+                        </Text>
+
+                    </View>
+                    <View className="flex-row justify-center items-center m-4">
+                        <Text className="text-white text-xl font-bold justify-center items-center">
+                            Login your account
+                        </Text>
+
+                    </View>
+                </View>
+                <View className="px-4 mt-4">
+                    <Text className="text-white font-semibold pb-2">Email</Text>
+                    <View className="bg-neutral-700 text-white px-4 py-2 rounded-lg flex-row items-center mb-4"> 
+                        <EnvelopeIcon size="24" color="gray"/>
+                        <TextInput
+                            value={email}
+                            onChangeText={value => setEmail(value)}
+                            placeholder="Email"
+                            placeholderTextColor="gray"
+                            className="flex-1 ml-4 text-white"                    
+                        />
+                    </View>
+
+                    <Text className="text-white font-semibold pb-2">Password</Text>
+                    <View className="bg-neutral-700 text-white px-4 py-2 rounded-lg flex-row items-center mb-4">
+                        <KeyIcon size="24" color="gray"/>
+                        <TextInput
+                            value={password}
+                            onChangeText={value => setPassword(value)}
+                            placeholder="Password"
+                            placeholderTextColor="gray"
+                            secureTextEntry={!showPassword}
+                            className="flex-1 ml-4 text-white"
+                        />
+                        <TouchableOpacity onPress={togglePasswordVisibility}>
+                            {showPassword ? <EyeSlashIcon size="24" color="gray" /> : <EyeIcon size="24" color="gray" />}
+                        </TouchableOpacity>
+                    </View>
+
+                    {/* error message */}
+                    <Text className="text-red-700 text-center">{error}</Text>
+
+                    <TouchableOpacity>
+                        <Text className="text-right text-white font-semibold">Forgot your password?</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity onPress={handleLogin} style={styles.backgroundButton} className="px-4 py-3 rounded-lg mt-6 mb-6">
+                        <Text className="text-white text-center text-base font-semibold">Log in</Text>
+                    </TouchableOpacity>
+                    <Text className="text-white text-center text-md font-semibold mb-6 ">Or Log in with </Text>
+                    <TouchableOpacity onPress={handleLoginWithGoogle} className="bg-red-600 px-4 py-3 rounded-lg mb-2">
+                        <Text className="text-white text-center text-base font-semibold">Login with Google</Text>
+                    </TouchableOpacity>
+                    
+                </View>
+
+                <View className="flex-row justify-center items-center mx-4 pt-3 mb-6">
+                    <Text className="text-white">Don't have an Account?</Text>
+                    <TouchableOpacity onPress={()=> navigation.navigate('SignUp')}>
+                        <Text className="text-white font-bold"> Sign Up</Text>
                     </TouchableOpacity>
                 </View>
-            </SafeAreaView>
-            <View className="mb-2 mt-3">
-                <View className="flex-row justify-center items-center mx-4">
-                    <Text className="text-white text-3xl font-bold justify-center items-center">
-                        <Text style={styles.text}>W</Text>elcome!
-                    </Text>
-
-                </View>
-                <View className="flex-row justify-center items-center m-4">
-                    <Text className="text-white text-xl font-bold justify-center items-center">
-                        Login your account
-                    </Text>
-
-                </View>
             </View>
-            <View className="px-4 mt-4">
-                <Text className="text-white font-semibold pb-2">Email</Text>
-                <View className="bg-neutral-700 text-white px-4 py-2 rounded-lg flex-row items-center mb-4"> 
-                    <EnvelopeIcon size="24" color="gray"/>
-                    <TextInput
-                        value={email}
-                        onChangeText={value => setEmail(value)}
-                        placeholder="Email"
-                        placeholderTextColor="gray"
-                        className="flex-1 ml-4 text-white"                    
-                    />
-                </View>
-
-                <Text className="text-white font-semibold pb-2">Password</Text>
-                <View className="bg-neutral-700 text-white px-4 py-2 rounded-lg flex-row items-center mb-4">
-                    <KeyIcon size="24" color="gray"/>
-                    <TextInput
-                        value={password}
-                        onChangeText={value => setPassword(value)}
-                        placeholder="Password"
-                        placeholderTextColor="gray"
-                        secureTextEntry={!showPassword}
-                        className="flex-1 ml-4 text-white"
-                    />
-                    <TouchableOpacity onPress={togglePasswordVisibility}>
-                        {showPassword ? <EyeSlashIcon size="24" color="gray" /> : <EyeIcon size="24" color="gray" />}
-                    </TouchableOpacity>
-                </View>
-
-                {/* error message */}
-                <Text className="text-red-700 text-center">{error}</Text>
-
-                <TouchableOpacity>
-                    <Text className="text-right text-white font-semibold">Forgot your password?</Text>
-                </TouchableOpacity>
-                <TouchableOpacity onPress={handleLogin} style={styles.backgroundButton} className="px-4 py-3 rounded-lg mt-6 mb-6">
-                    <Text className="text-white text-center text-lg font-semibold">Log in</Text>
-                </TouchableOpacity>
-                <Text className="text-white text-center text-md font-semibold mb-6 ">Or Log in with </Text>
-                <TouchableOpacity onPress={handleLoginWithGoogle} className="bg-red-600 px-4 py-3 rounded-lg mb-2">
-                    <Text className="text-white text-center font-semibold">Login with Google</Text>
-                </TouchableOpacity>
-                <TouchableOpacity onPress={handleLoginWithFacebook} className="bg-blue-400 px-4 py-3 rounded-lg">
-                    <Text className="text-white text-center font-semibold">Login with Facebook</Text>
-                </TouchableOpacity>
-
-                
-            </View>
-
-            <View className="flex-row justify-center items-center mx-4 pt-6">
-                <Text className="text-white">Don't have an Account?</Text>
-                <TouchableOpacity onPress={()=> navigation.navigate('SignUp')}>
-                    <Text className="text-white font-bold"> Sign Up</Text>
-                </TouchableOpacity>
-            </View>
-        </View>
+        </KeyboardAvoidingView>
     );
 }
--- screens/SignupScreen.js
+++ screens/SignupScreen.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { View, Text, TextInput, TouchableOpacity, Alert } from 'react-native';
+import { View, Text, TextInput, TouchableOpacity, Alert, KeyboardAvoidingView, Platform } from 'react-native';
 import { KeyIcon, EnvelopeIcon, UserIcon, EyeIcon, EyeSlashIcon } from 'react-native-heroicons/outline';
 import { useNavigation } from '@react-navigation/native';
 import { styles } from '../theme';
@@ -41,7 +41,13 @@ export default function SignUpScreen() {
     }
 
     const handleSignUp = async () => {
-        if (displayName && email && password && confirmPassword && password === confirmPassword) {
+        if (!displayName || !email || !password || !confirmPassword) {
+            setError('Please fill in all fields');
+        } else if (!validateEmail(email)) {
+            setError('Invalid email address');
+        } else if (password !== confirmPassword) {
+            setError('Passwords do not match');
+        } else {
             try {
                 const userCredential = await createUserWithEmailAndPassword(auth, email, password);
                 const user = userCredential.user;
@@ -49,7 +55,7 @@ export default function SignUpScreen() {
                 if (user) {
                     // Cập nhật displayName vào Authentication
                     await updateProfile(user, { displayName: displayName });
-                    
+    
                     // Chuyển hướng người dùng đến trang Home sau khi đăng ký thành công
                     navigation.navigate('Home');
                 }
@@ -63,6 +69,7 @@ export default function SignUpScreen() {
             }
         }
     };
+    
 
     const togglePasswordVisibility = () => {
         setShowPassword(!showPassword);
@@ -73,88 +80,94 @@ export default function SignUpScreen() {
 
 
     return (
-        <View className="flex-1 bg-neutral-800 pt-6">
-            <View className="mb-2 mt-6">
-                <View className="flex-row justify-center items-center mx-4">
-                    <Text className="text-white text-3xl font-bold justify-center items-center">
-                        <Text style={styles.text}>S</Text>ign Up!
-                    </Text>
+        <KeyboardAvoidingView behavior={Platform.OS === 'ios' ? 'padding' : 'height'} className="flex-1">
+            <View className="flex-1 bg-neutral-800">
+                <View className="flex-1"></View>
+                <View className="flex-1">
+                    <View className="flex-row justify-center items-center mx-4">
+                        <Text className="text-white text-3xl font-bold justify-center items-center">
+                            <Text style={styles.text}>S</Text>ign Up!
+                        </Text>
+                    </View>
+                    <View className="flex-row justify-center items-center m-4">
+                        <Text className="text-white text-xl font-bold justify-center items-center">
+                            Create your account
+                        </Text>
+                    </View>
                 </View>
-                <View className="flex-row justify-center items-center m-4">
-                    <Text className="text-white text-xl font-bold justify-center items-center">
-                        Create your account
-                    </Text>
-                </View>
-            </View>
-            <View className="px-4 mt-6">
-                <Text className="text-white font-semibold pb-2">Full Name</Text>
-                <View className="bg-neutral-700 text-white px-4 py-2 rounded-lg flex-row items-center mb-4">
-                    <UserIcon size={24} color="gray" />
-                    <TextInput
-                        value={displayName}
-                        onChangeText={value => setDisplayName(value)}
-                        placeholder="FullName"
-                        placeholderTextColor="gray"
-                        className="flex-1 ml-4 text-white"
-                    />
+                <View className="px-4 pt-8 mt-2">
+                    <Text className="text-white font-semibold pb-2">Full Name</Text>
+                    <View className="bg-neutral-700 text-white px-4 py-2 rounded-lg flex-row items-center mb-4">
+                        <UserIcon size={24} color="gray" />
+                        <TextInput
+                            value={displayName}
+                            onChangeText={value => setDisplayName(value)}
+                            placeholder="FullName"
+                            placeholderTextColor="gray"
+                            className="flex-1 ml-4 text-white"
+                        />
+                    </View>
+
+                    <Text className="text-white font-semibold pb-2">Email</Text>
+                    <View className="bg-neutral-700 text-white px-4 py-2 rounded-lg flex-row items-center mb-4">
+                        <EnvelopeIcon size={24} color="gray" />
+                        <TextInput
+                            value={email}
+                            onChangeText={value => setEmail(value)}
+                            placeholder="Email"
+                            placeholderTextColor="gray"
+                            className="flex-1 ml-4 text-white"
+                        />
+                    </View>
+
+                    <Text className="text-white font-semibold pb-2">Password</Text>
+                    <View className="bg-neutral-700 text-white px-4 py-2 rounded-lg flex-row items-center mb-4">
+                        <KeyIcon size={24} color="gray" />
+                        <TextInput
+                            value={password}
+                            onChangeText={value => setPassword(value)}
+                            placeholder="Password"
+                            placeholderTextColor="gray"
+                            secureTextEntry={!showPassword}
+                            className="flex-1 ml-4 text-white"
+                        />
+                        <TouchableOpacity onPress={togglePasswordVisibility}>
+                            {showPassword ? <EyeSlashIcon size="24" color="gray" /> : <EyeIcon size="24" color="gray" />}
+                        </TouchableOpacity>
+                    </View>
+
+                    <Text className="text-white font-semibold pb-2">Confirm Password</Text>
+                    <View className="bg-neutral-700 text-white px-4 py-2 rounded-lg flex-row items-center mb-2">
+                        <KeyIcon size={24} color="gray" />
+                        <TextInput
+                            value={confirmPassword}
+                            onChangeText={value => setConfirmPassword(value)}
+                            placeholder="Confirm Password"
+                            placeholderTextColor="gray"
+                            secureTextEntry={!showConfirmPassword}
+                            className="flex-1 ml-4 text-white"
+                        />
+                        <TouchableOpacity onPress={toggleConfirmPasswordVisibility}>
+                            {showConfirmPassword ? <EyeSlashIcon size="24" color="gray" /> : <EyeIcon size="24" color="gray" />}
+                        </TouchableOpacity>
+                    </View>
+
+                    <Text className="text-red-600 text-center text-base">{error}</Text>                    
                 </View>
 
-                <Text className="text-white font-semibold pb-2">Email</Text>
-                <View className="bg-neutral-700 text-white px-4 py-2 rounded-lg flex-row items-center mb-4">
-                    <EnvelopeIcon size={24} color="gray" />
-                    <TextInput
-                        value={email}
-                        onChangeText={value => setEmail(value)}
-                        placeholder="Email"
-                        placeholderTextColor="gray"
-                        className="flex-1 ml-4 text-white"
-                    />
-                </View>
-
-                <Text className="text-white font-semibold pb-2">Password</Text>
-                <View className="bg-neutral-700 text-white px-4 py-2 rounded-lg flex-row items-center mb-4">
-                    <KeyIcon size={24} color="gray" />
-                    <TextInput
-                        value={password}
-                        onChangeText={value => setPassword(value)}
-                        placeholder="Password"
-                        placeholderTextColor="gray"
-                        secureTextEntry={!showPassword}
-                        className="flex-1 ml-4 text-white"
-                    />
-                    <TouchableOpacity onPress={togglePasswordVisibility}>
-                        {showPassword ? <EyeSlashIcon size="24" color="gray" /> : <EyeIcon size="24" color="gray" />}
+                <View className="px-4 mt-2">
+                    <TouchableOpacity onPress={handleSignUp} style={styles.backgroundButton} className="px-4 py-3 rounded-lg mt-2 mb-6">
+                        <Text className="text-white text-center text-base font-semibold">Sign Up</Text>
                     </TouchableOpacity>
                 </View>
 
-                <Text className="text-white font-semibold pb-2">Confirm Password</Text>
-                <View className="bg-neutral-700 text-white px-4 py-2 rounded-lg flex-row items-center mb-4">
-                    <KeyIcon size={24} color="gray" />
-                    <TextInput
-                        value={confirmPassword}
-                        onChangeText={value => setConfirmPassword(value)}
-                        placeholder="Confirm Password"
-                        placeholderTextColor="gray"
-                        secureTextEntry={!showConfirmPassword}
-                        className="flex-1 ml-4 text-white"
-                    />
-                    <TouchableOpacity onPress={toggleConfirmPasswordVisibility}>
-                        {showConfirmPassword ? <EyeSlashIcon size="24" color="gray" /> : <EyeIcon size="24" color="gray" />}
+                <View className="flex-row justify-center items-center mx-4 pb-6">
+                    <Text className="text-white">Already have an account?</Text>
+                    <TouchableOpacity onPress={() => navigation.navigate('Login')}>
+                        <Text className="text-white font-bold"> Log In</Text>
                     </TouchableOpacity>
                 </View>
-
-                <Text className="text-red-700 text-center">{error}</Text>
-
-                <TouchableOpacity onPress={handleSignUp} style={styles.backgroundButton} className="px-4 py-3 rounded-lg mt-6 mb-6">
-                    <Text className="text-white text-center text-lg font-semibold">Sign Up</Text>
-                </TouchableOpacity>
             </View>
-            <View className="flex-row justify-center items-center mx-4 pt-6">
-                <Text className="text-white">Already have an account?</Text>
-                <TouchableOpacity onPress={() => navigation.navigate('Login')}>
-                    <Text className="text-white font-bold"> Log In</Text>
-                </TouchableOpacity>
-            </View>
-        </View>
+        </KeyboardAvoidingView>
     );
 }
